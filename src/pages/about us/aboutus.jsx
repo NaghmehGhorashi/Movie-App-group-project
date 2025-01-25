4949ea42f8912aefd53b958bad2637a4fdcf4a2f
Raana <raana.barimani@hyperisland.se>
2025-01-25 15:36:04 +0100

    fixed 2 image

--- src/pages/about us/aboutus.jsx
+++ src/pages/about us/aboutus.jsx
@@ -2,16 +2,37 @@ import React from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Wrapper from '../../components/container/container'
 import style from "./aboutus.module.css"
-import naghmeh from "../../assets/naghmeh.jpeg"
-
+import hannaImage from '../../assets/hanna.jpeg'
+import naghmehImage from '../../assets/naghmeh.jpeg'
 function Aboutus() {
+    
     return (
         <div>
             <Wrapper>
                 <div>
                     <Navbar />
-                    <h1>about</h1>
-                    {/* write ur code here */}
+                    <h1>About</h1>
+                    {/* Profile Section */}
+                    <section class="profile-section">
+                        <div class="profile">
+                            <img src = {hannaImage} alt="Profile 1"/>
+                            <p>Hanna</p>
+                        </div>
+                        
+                            <div class="profile">
+                            <img src={naghmehImage} alt="Profile 2"/>
+                            <p>Nghmeh</p>
+                        </div>
+
+                        <div class="profile">
+                            <img src="https://via.placeholder.com/100" alt="Profile 3"/>
+                            <p>Profile 3</p>
+                        </div>
+                        <div class="profile">
+                            {/* <img src="https://via.placeholder.com/100" alt="Profile 4"> */}
+                            <p>Profile 4</p>
+                        </div>
+                    </section>
                     <div className={style.about}>
 
                     </div>
