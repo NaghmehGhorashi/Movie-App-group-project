e357e4619025de316187b67b1183a18cc2d6b320
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-01-26 20:17:35 +0100

    fix comment for sixten,add some style for container

--- src/pages/add/add.jsx
+++ src/pages/add/add.jsx
@@ -4,6 +4,9 @@ import Wrapper from '../../components/container/container'
 import style from "./add.module.css"
 
 function Add() {
+
+
+    {/* write ur code here */ }
     return (
 
         <div>
@@ -28,8 +31,8 @@ export default Add
 
 {/* When you want to create the submit button, use : <<aria-label="Add">> and <<tabIndex="0" >> */ }
 // Since we are using useContext,
-//  you should define the add function inside the context.
+//  you should define the addhandle function inside the context.
 //   Then, you can call it from your component using the context. 
 //   This approach helps centralize logic, makes the code cleaner, 
 //   and avoids passing props unnecessarily. 
-//   example:<button onClick={handleAdd}>Add Item</button>
+//  
